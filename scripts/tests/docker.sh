f09c58ba76a406b54a5d5cd8b3dcda0392c3f581
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-04 16:47:46 -0700

    scripts/tests: add python test script in Docker
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/tests/docker.sh
+++ scripts/tests/docker.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-<<COMMENT
-https://www.tensorflow.org/install/install_mac
-COMMENT
-
-if ! [[ "$0" =~ "./scripts/tests/go-docker.sh" ]]; then
+if ! [[ "$0" =~ "./scripts/tests/docker.sh" ]]; then
   echo "must be run from repository root"
   exit 255
 fi
 
-docker run --rm \
+docker run \
+  --rm \
   --volume=`pwd`:/gopath/src/github.com/gyuho/deephardway \
   gcr.io/deephardway/deephardway:latest \
   ./scripts/tests/go.sh
+
+docker run \
+  --rm \
+  --volume=`pwd`:/gopath/src/github.com/gyuho/deephardway \
+  gcr.io/deephardway/deephardway:latest \
+  /bin/sh -c "ETCD_TEST_PATH='/etcd' ./scripts/tests/python.sh"
 
 <<COMMENT
 docker run --rm \
