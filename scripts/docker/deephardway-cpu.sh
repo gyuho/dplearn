d3773f14c2d7db5b62e2cdf9ff2208dd67e20639
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-05 14:49:52 -0700

    scripts: do not expose :80 (use proxy instead later)
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/docker/deephardway-cpu.sh
+++ scripts/docker/deephardway-cpu.sh
@@ -13,7 +13,6 @@ fi
 docker run \
   --rm \
   -it \
-  -p 80:80 \
   -p 4200:4200 \
   gcr.io/deephardway/deephardway:latest-cpu \
   /bin/sh -c "pushd /gopath/src/github.com/gyuho/deephardway && ./scripts/run/deephardway-cpu.sh"
