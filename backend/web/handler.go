479536f90bbe44db8e204bf7d267420f5766cece
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-07 00:02:19 -0700

    backend/web: add 'watch' method, return on context cancel
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- backend/web/handler.go
+++ backend/web/handler.go
@@ -35,38 +35,41 @@ func wordPredictHandler(ctx context.Context, w http.ResponseWriter, req *http.Re
 			})
 		}
 
-		qu := ctx.Value(queueKey).(etcdqueue.Queue)
 		userID := ctx.Value(userKey).(string)
+		requestID := userID + req.URL.Path
 
+		wordPredictMu.RLock()
+		item, ok := wordPredictRequests[requestID]
+		wordPredictMu.RUnlock()
+		if ok {
+			return json.NewEncoder(w).Encode(item)
+		}
+
+		// 1. create a new Item
 		creq.UserID = userID
 		creq.Result = ""
 		rb, err = json.Marshal(creq)
 		if err != nil {
 			return err
 		}
-		item := etcdqueue.CreateItem("cats-vs-dogs", 100, string(rb))
+		item = etcdqueue.CreateItem(req.URL.Path, 100, string(rb))
 
-		// TODO: write to queue
-		fmt.Println("queue:", qu.ClientEndpoints())
-
-		cnt := 0
-		for item.Progress < 100 {
-			// TODO: watch from queue until it's done
-			time.Sleep(time.Second)
-			creq.Result = fmt.Sprintf("Processing %+v at %s", creq, time.Now().String()[:29])
-			rb, err = json.Marshal(creq)
-			if err != nil {
-				return err
-			}
-			item.Value = string(rb)
-			item.Progress = (cnt + 1) * 10
-			cnt++
-
-			// received progress report from queue service
-			if err := json.NewEncoder(w).Encode(item); err != nil {
-				return err
-			}
+		// 2. enqueue(schedule) the job
+		qu := ctx.Value(queueKey).(etcdqueue.Queue)
+		ch, err := qu.Add(ctx, item)
+		if err != nil {
+			return json.NewEncoder(w).Encode(&etcdqueue.Item{
+				Progress: 0,
+				Error:    fmt.Errorf("Queue.Add error %q at %s", err.Error(), time.Now().String()[:29]),
+			})
 		}
+
+		// 3. watch for changes for later request polling
+		wordPredictMu.Lock()
+		wordPredictRequests[requestID] = item
+		wordPredictMu.Unlock()
+
+		go watch(ctx, requestID, item, creq, ch)
 
 	default:
 		http.Error(w, "Method Not Allowed", 405)
@@ -97,38 +100,41 @@ func catsVsDogsHandler(ctx context.Context, w http.ResponseWriter, req *http.Req
 			})
 		}
 
-		qu := ctx.Value(queueKey).(etcdqueue.Queue)
 		userID := ctx.Value(userKey).(string)
+		requestID := userID + req.URL.Path
 
+		catsVsDogsMu.RLock()
+		item, ok := catsVsDogsRequests[requestID]
+		catsVsDogsMu.RUnlock()
+		if ok {
+			return json.NewEncoder(w).Encode(item)
+		}
+
+		// 1. create a new Item
 		creq.UserID = userID
 		creq.Result = ""
 		rb, err = json.Marshal(creq)
 		if err != nil {
 			return err
 		}
-		item := etcdqueue.CreateItem("cats-vs-dogs", 100, string(rb))
+		item = etcdqueue.CreateItem(req.URL.Path, 100, string(rb))
 
-		// TODO: write to queue
-		fmt.Println("queue:", qu.ClientEndpoints())
-
-		cnt := 0
-		for item.Progress < 100 {
-			// TODO: watch from queue until it's done
-			time.Sleep(time.Second)
-			creq.Result = fmt.Sprintf("Processing %+v at %s", creq, time.Now().String()[:29])
-			rb, err = json.Marshal(creq)
-			if err != nil {
-				return err
-			}
-			item.Value = string(rb)
-			item.Progress = (cnt + 1) * 10
-			cnt++
-
-			// received progress report from queue service
-			if err := json.NewEncoder(w).Encode(item); err != nil {
-				return err
-			}
+		// 2. enqueue(schedule) the job
+		qu := ctx.Value(queueKey).(etcdqueue.Queue)
+		ch, err := qu.Add(ctx, item)
+		if err != nil {
+			return json.NewEncoder(w).Encode(&etcdqueue.Item{
+				Progress: 0,
+				Error:    fmt.Errorf("Queue.Add error %q at %s", err.Error(), time.Now().String()[:29]),
+			})
 		}
+
+		// 3. watch for changes for later request polling
+		catsVsDogsMu.Lock()
+		catsVsDogsRequests[requestID] = item
+		catsVsDogsMu.Unlock()
+
+		go watch(ctx, requestID, item, creq, ch)
 
 	default:
 		http.Error(w, "Method Not Allowed", 405)
@@ -193,34 +199,43 @@ func mnistHandler(ctx context.Context, w http.ResponseWriter, req *http.Request)
 		mnistRequests[requestID] = item
 		mnistMu.Unlock()
 
-		go func(ctx context.Context, id string, item *etcdqueue.Item, req Request, ch <-chan *etcdqueue.Item) {
-			cnt := 0
-			for item.Progress < 100 {
-				// TODO: watch from queue until it's done, feed from queue service
-				time.Sleep(time.Second)
-				req.Result = fmt.Sprintf("Processing %+v at %s", req, time.Now().String()[:29])
-				rb, err = json.Marshal(req)
-				if err != nil {
-					panic(err)
-				}
-				item.Value = string(rb)
-				item.Progress = (cnt + 1) * 10
-				cnt++
-
-				mnistMu.Lock()
-				mnistRequests[requestID] = item
-				mnistMu.Unlock()
-
-				select {
-				case <-ctx.Done():
-					return
-				}
-			}
-		}(ctx, requestID, item, creq, ch)
+		go watch(ctx, requestID, item, creq, ch)
 
 	default:
 		http.Error(w, "Method Not Allowed", 405)
 	}
 
 	return nil
+}
+
+func watch(ctx context.Context, requestID string, item *etcdqueue.Item, req Request, ch <-chan *etcdqueue.Item) {
+	cnt := 0
+	for item.Progress < 100 {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		// TODO: watch from queue until it's done, feed from queue service
+		time.Sleep(time.Second)
+		req.Result = fmt.Sprintf("Processing %+v at %s", req, time.Now().String()[:29])
+		rb, err := json.Marshal(req)
+		if err != nil {
+			panic(err)
+		}
+		item.Value = string(rb)
+		item.Progress = (cnt + 1) * 10
+		cnt++
+
+		mnistMu.Lock()
+		mnistRequests[requestID] = item
+		mnistMu.Unlock()
+
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+	}
 }
