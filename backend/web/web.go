e38990f61ced36f6f87e791b09878308fa60b2eb
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-08 13:25:45 -0700

    backend/web: combine handlers
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- backend/web/web.go
+++ backend/web/web.go
@@ -2,7 +2,9 @@ package web
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -38,19 +40,19 @@ func StartServer(webPort, queuePort int, dataDir string) (*Server, error) {
 	mux := http.NewServeMux()
 	mux.Handle("/word-predict-request-1", &ContextAdapter{
 		ctx:     rootCtx,
-		handler: with(ContextHandlerFunc(wordPredictHandler), qu),
+		handler: with(ContextHandlerFunc(clientRequestHandler), qu),
 	})
 	mux.Handle("/word-predict-request-2", &ContextAdapter{
 		ctx:     rootCtx,
-		handler: with(ContextHandlerFunc(wordPredictHandler), qu),
+		handler: with(ContextHandlerFunc(clientRequestHandler), qu),
 	})
 	mux.Handle("/cats-vs-dogs-request", &ContextAdapter{
 		ctx:     rootCtx,
-		handler: with(ContextHandlerFunc(catsVsDogsHandler), qu),
+		handler: with(ContextHandlerFunc(clientRequestHandler), qu),
 	})
 	mux.Handle("/mnist-request", &ContextAdapter{
 		ctx:     rootCtx,
-		handler: with(ContextHandlerFunc(mnistHandler), qu),
+		handler: with(ContextHandlerFunc(clientRequestHandler), qu),
 	})
 
 	addrURL := url.URL{Scheme: "http", Host: fmt.Sprintf("localhost:%d", webPort)}
@@ -140,4 +142,133 @@ type Request struct {
 	URL     string `json:"url"`
 	RawData string `json:"rawdata"`
 	Result  string `json:"result"`
+}
+
+var (
+	requestCacheMu sync.RWMutex
+	requestCache   = make(map[string]*etcdqueue.Item)
+)
+
+func clientRequestHandler(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
+	switch req.Method {
+	case http.MethodPost:
+		rb, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return err
+		}
+		req.Body.Close()
+
+		creq := Request{}
+		if err = json.Unmarshal(rb, &creq); err != nil {
+			return json.NewEncoder(w).Encode(&etcdqueue.Item{
+				Progress: 0,
+				Error:    fmt.Errorf("JSON parse error %q at %s", err.Error(), time.Now().String()[:29]),
+			})
+		}
+
+		userID := ctx.Value(userKey).(string)
+		requestID := fmt.Sprintf("%s-%s-%s", userID, req.URL.Path, hashSha512(creq.RawData))
+
+		requestCacheMu.RLock()
+		item, ok := requestCache[requestID]
+		requestCacheMu.RUnlock()
+		if ok {
+			return json.NewEncoder(w).Encode(item)
+		}
+
+		glog.Infof("creating a new item with request ID %s", requestID)
+		creq.UserID = userID
+		creq.Result = ""
+		rb, err = json.Marshal(creq)
+		if err != nil {
+			return err
+		}
+		item = etcdqueue.CreateItem(req.URL.Path, 100, string(rb))
+
+		// 2. enqueue(schedule) the job
+		qu := ctx.Value(queueKey).(etcdqueue.Queue)
+		ch, err := qu.Add(ctx, item)
+		if err != nil {
+			return json.NewEncoder(w).Encode(&etcdqueue.Item{
+				Progress: 0,
+				Error:    fmt.Errorf("Queue.Add error %q at %s", err.Error(), time.Now().String()[:29]),
+			})
+		}
+
+		// 3. watch for changes for later request polling
+		requestCacheMu.Lock()
+		requestCache[requestID] = item
+		requestCacheMu.Unlock()
+
+		go watch(ctx, requestID, item, creq, ch)
+
+	case http.MethodDelete:
+		rb, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return err
+		}
+		req.Body.Close()
+
+		creq := Request{}
+		if err = json.Unmarshal(rb, &creq); err != nil {
+			return json.NewEncoder(w).Encode(&etcdqueue.Item{
+				Progress: 0,
+				Error:    fmt.Errorf("JSON parse error %q at %s", err.Error(), time.Now().String()[:29]),
+			})
+		}
+
+		userID := ctx.Value(userKey).(string)
+		requestID := fmt.Sprintf("%s-%s-%s", userID, req.URL.Path, hashSha512(creq.RawData))
+
+		glog.Infof("requested to delete %q", requestID)
+		requestCacheMu.Lock()
+		delete(requestCache, requestID)
+		requestCacheMu.Unlock()
+		glog.Infof("deleted %q", requestID)
+
+	default:
+		http.Error(w, "Method Not Allowed", 405)
+	}
+
+	return nil
+}
+
+func watch(ctx context.Context, requestID string, item *etcdqueue.Item, req Request, ch <-chan *etcdqueue.Item) {
+	cnt := 0
+	for item.Progress < 100 {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		requestCacheMu.RLock()
+		_, ok := requestCache[requestID]
+		requestCacheMu.RUnlock()
+		if !ok {
+			glog.Infof("%q is deleted; exiting watch routine", requestID)
+		}
+
+		// TODO: watch from queue until it's done, feed from queue service
+		time.Sleep(time.Second)
+		req.Result = fmt.Sprintf("Processing %+v at %s", req, time.Now().String()[:29])
+		rb, err := json.Marshal(req)
+		if err != nil {
+			panic(err)
+		}
+		item.Value = string(rb)
+		item.Progress = (cnt + 1) * 10
+		cnt++
+
+		requestCacheMu.Lock()
+		requestCache[requestID] = item
+		requestCacheMu.Unlock()
+		glog.Infof("updated item with request ID %s", requestID)
+
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+	}
 }
