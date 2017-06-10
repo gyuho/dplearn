18d61b17e868e29f1311448fc9576e9f73011561
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-10 01:43:39 -0700

    frontend/app: update 'Item' class with new fields
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- frontend/app/request-item.component.ts
+++ frontend/app/request-item.component.ts
@@ -1,17 +1,17 @@
 
 // Request represents TypeScript version of Request in https://github.com/gyuho/deephardway/blob/master/backend/web/web.go.
 export class Request {
-  userid: string;
+  user_id: string;
   url: string;
-  rawdata: string;
+  raw_data: string;
   result: string;
   constructor(
     url: string,
-    rawdata: string,
+    d: string,
   ) {
-    this.userid = '';
+    this.user_id = '';
     this.url = url;
-    this.rawdata = rawdata;
+    this.raw_data = d;
     this.result = '';
   }
 };
@@ -19,9 +19,11 @@ export class Request {
 // Item represents TypeScript version of Item in https://github.com/gyuho/deephardway/blob/master/pkg/etcd-queue/queue.go.
 export class Item {
   bucket: string;
+  created_at: string;
   key: string;
   value: string;
   progress: number;
+  canceled: boolean;
   error: string;
   constructor(
     bucket: string,
@@ -31,9 +33,11 @@ export class Item {
     error: string,
   ) {
     this.bucket = bucket;
+    this.created_at = '';
     this.key = key;
     this.value = value;
     this.progress = progress;
+    this.canceled = false;
     this.error = error;
   }
 };
