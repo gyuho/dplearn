925258b7c7041b65276bfa3c62389fd0d8464226
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-13 03:09:20 -0700

    scripts/dep: add 'smart-rename' flag

--- scripts/dep/download-data.sh
+++ scripts/dep/download-data.sh
@@ -12,8 +12,10 @@ download-data -source-path http://files.fast.ai/data/dogscats.zip \
   -target-path $HOME/data/deephardway.data/dogscats.zip \
   -output-dir $HOME/data/deephardway.data/dogscats \
   -output-dir-overwrite \
+  -smart-rename \
   -logtostderr
 
 download-data -source-path http://files.fast.ai/models/vgg16.h5 \
   -target-path $HOME/data/deephardway.data/vgg16.h5 \
+  -verbose \
   -logtostderr
