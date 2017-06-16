5f4551b38338d3b10cae5d5e9101217224869d32
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-15 23:54:04 -0700

    scripts/dep: move package-json.sh
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/dep/package-json.sh
+++ scripts/dep/package-json.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if ! [[ "$0" =~ "./scripts/gen-package-json.sh" ]]; then
+if ! [[ "$0" =~ "./scripts/dep/package-json.sh" ]]; then
   echo "must be run from repository root"
   exit 255
 fi
