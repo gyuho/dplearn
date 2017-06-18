8a80a96a42f8735332f7af0a6af9f12c79f7a55f
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-18 00:51:28 -0700

    scripts/run: sleep before run worker
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/run/deephardway-gpu.sh
+++ scripts/run/deephardway-gpu.sh
@@ -13,7 +13,8 @@ gen-nginx-conf -output nginx.conf -target-port 4200 -logtostderr=true
 cat nginx.conf
 
 backend-web-server -web-port 2200 -queue-port-client 22000 -queue-port-peer 22001 -data-dir /var/lib/etcd -logtostderr=true &
+yarn start-prod &
+sleep 5s
 python ./backend/worker/worker.py http://localhost:2200/cats-vs-dogs-request/queue &
 python ./backend/worker/worker.py http://localhost:2200/word-predict-request/queue &
-yarn start-prod &
 wait
