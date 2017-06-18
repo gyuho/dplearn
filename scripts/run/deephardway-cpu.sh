d470a8b48ec8696a4933395a69fc3d3944c6b9ec
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-18 02:46:00 -0700

    scripts/run: add 'word-predict' endpoint example
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/run/deephardway-cpu.sh
+++ scripts/run/deephardway-cpu.sh
@@ -44,5 +44,6 @@ sleep 5s
 
 ETCDCTL_API=3 /etcdctl --endpoints=localhost:22000 get "" --from-key
 
+python ./backend/worker/worker.py http://localhost:2200/word-predict-request/queue
 python ./backend/worker/worker.py http://localhost:2200/cats-vs-dogs-request/queue
 COMMENT
