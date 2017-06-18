3b9539fdecd3b9a52d84164750686fd28839cf68
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-17 21:32:55 -0700

    scripts/run: run worker
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/run/deephardway-cpu.sh
+++ scripts/run/deephardway-cpu.sh
@@ -13,5 +13,35 @@ gen-nginx-conf -output nginx.conf -target-port 4200 -logtostderr=true
 cat nginx.conf
 
 backend-web-server -web-port 2200 -queue-port-client 22000 -queue-port-peer 22001 -data-dir /var/lib/etcd -logtostderr=true &
+python ./backend/worker/worker.py http://localhost:2200/cats-vs-dogs-request/queue &
+python ./backend/worker/worker.py http://localhost:2200/word-predict-request/queue &
 yarn start-prod &
 wait
+
+<<COMMENT
+rm -rf /tmp/etcd
+go install -v ./cmd/backend-web-server
+backend-web-server -web-port 2200 -queue-port-client 22000 -queue-port-peer 22001 -data-dir /tmp/etcd -logtostderr=true
+
+ETCDCTL_API=3 /etcdctl --endpoints=localhost:22000 get "" --from-key
+
+curl -L http://localhost:2200/cats-vs-dogs-request/queue
+
+curl -L http://localhost:2200/cats-vs-dogs-request/queue \
+  -H "Content-Type: application/json" \
+  -X POST -d '{}'
+
+curl -L http://localhost:2200/cats-vs-dogs-request/queue \
+  -H "Content-Type: application/json" \
+  -X POST -d '{"bucket" : "/cats-vs-dogs-request", "key" : "/cats-vs-dogs-request", "value" : ""}'
+
+curl -L http://localhost:2200/cats-vs-dogs-request/queue \
+  -H "Content-Type: application/json" \
+  -X POST -d '{"bucket" : "/cats-vs-dogs-request", "key" : "/cats-vs-dogs-request", "value" : "bar"}'
+
+sleep 5s
+
+ETCDCTL_API=3 /etcdctl --endpoints=localhost:22000 get "" --from-key
+
+python ./backend/worker/worker.py http://localhost:2200/cats-vs-dogs-request/queue
+COMMENT
