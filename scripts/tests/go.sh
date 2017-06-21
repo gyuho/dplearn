69e70fe38ea50bc94ebc0b9a3bc754947679a43c
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-20 17:22:11 -0700

    scripts/tests: fix 'go vet' commands, check race conditions
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/tests/go.sh
+++ scripts/tests/go.sh
@@ -6,21 +6,23 @@ if ! [[ "$0" =~ "./scripts/tests/go.sh" ]]; then
   exit 255
 fi
 
-IGNORE_PKGS="(vendor|tensorflow|node_modules)"
+IGNORE_PKGS="(vendor|node_modules)"
 TESTS=`find . -name \*_test.go | while read a; do dirname $a; done | sort | uniq | egrep -v "$IGNORE_PKGS"`
 
 echo "Checking gofmt..." $TESTS
 fmtRes=$(gofmt -l -s -d $TESTS)
-if [ -n "${fmtRes}" ]; then
+if [[ "${fmtRes}" ]]; then
   echo -e "gofmt checking failed:\n${fmtRes}"
   exit 255
 fi
+
 echo "Checking govet..." $TESTS
-vetRes=$(go vet $TESTS 2>&1 >/dev/null)
-if [ -n "${vetRes}" ]; then
+vetRes=$(go vet $TESTS)
+if [[ "${vetRes}" ]]; then
   echo -e "govet checking failed:\n${vetRes}"
   exit 255
 fi
 
 echo "Running tests..." $TESTS
 go test -v $TESTS;
+go test -v -race $TESTS;
