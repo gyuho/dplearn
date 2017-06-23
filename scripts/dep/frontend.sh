3fb13de2e82fe50df161e49ea3b4f4d772e74b74
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-22 22:14:39 -0700

    *: use node v8.1.2, upgrade angular versions, regenerate dockerfiles

--- scripts/dep/frontend.sh
+++ scripts/dep/frontend.sh
@@ -7,13 +7,19 @@ if ! [[ "$0" =~ "./scripts/dep/frontend.sh" ]]; then
 fi
 
 source ${NVM_DIR}/nvm.sh
-nvm install v7.10.0
+nvm install v8.1.2
 
 go install -v ./cmd/gen-package-json
 gen-package-json --output package.json --logtostderr
 
 # npm install -g tslint
 yarn install
-npm rebuild node-sass
+npm rebuild node-sass --force
 yarn install
 npm install
+
+nvm install v8.1.2
+nvm alias default 8.1.2
+nvm alias default node
+which node
+node -v
