70dd3efb2eed4f1410d686bebf1d2f5338d3e4d9
Gyu-Ho Lee <gyuhox@gmail.com>
2017-06-25 21:50:50 -0700

    scripts/dep: remove lock.json
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/dep/frontend.sh
+++ scripts/dep/frontend.sh
@@ -13,11 +13,12 @@ go install -v ./cmd/gen-package-json
 gen-package-json --output package.json --logtostderr
 
 echo "Updating frontend dependencies with 'yarn' and 'npm'..."
-# npm install -g tslint
+rm -f ./package-lock.json
 yarn install
 npm rebuild node-sass --force
 yarn install
 npm install
+# npm install -g tslint
 
 nvm install v8.1.2
 nvm alias default 8.1.2
