cece3378167444e1a7ec18ce3c39aeb278f2cc28
Gyu-Ho Lee <gyuhox@gmail.com>
2017-07-21 16:07:50 -0700

    frontend/*: upgrade node, angular, angular-cli
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/dep/frontend.sh
+++ scripts/dep/frontend.sh
@@ -11,7 +11,7 @@ SKIP_REBUILD=1 ./scripts/dep/frontend.sh
 COMMENT
 
 source ${NVM_DIR}/nvm.sh
-nvm install v8.1.4
+nvm install v8.2.1
 
 go install -v ./cmd/gen-package-json
 gen-package-json --output package.json --logtostderr
@@ -28,8 +28,8 @@ else
 fi
 npm install
 
-nvm install v8.1.4
-nvm alias default 8.1.4
+nvm install v8.2.1
+nvm alias default 8.2.1
 nvm alias default node
 which node
 node -v
